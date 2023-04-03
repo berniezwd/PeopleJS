def7693472750e14863ae279e7625d8c408aaf09
zwd <bernieweidongz@outlook.com>
2023-04-03 09:00:45 +0800

    first commit

---
+++ init.sh
@@ -0,0 +1,7 @@
+echo "# PeopleJS" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:berniezwd/PeopleJS.git
+git push -u origin main
